d2806f521822322929250b005e57e6849c907ecd
Josh Buker <crypto@joshbuker.com>
2025-06-12 12:00:47 -0700

    Add detail to auto update script and change permissions
    Added detail on if the auto update script is rebooting or not
    Also made both host scripts executable

--- streaming/host_scripts/auto_update_everything.sh
+++ streaming/host_scripts/auto_update_everything.sh
@@ -15,6 +15,9 @@ docker image prune -f
 echo "$(date +"%F %T") Device updated" | sudo tee -a /var/log/auto_update_everything.log
 if [ -f /var/run/reboot-required ]
 then
+    echo "$(date +"%F %T") Device updated - Reboot required, restarting in 2 seconds" | sudo tee -a /var/log/auto_update_everything.log
     ( sleep 2 ; sudo reboot now) &
+else
+    echo "$(date +"%F %T") Device updated - No reboot needed" | sudo tee -a /var/log/auto_update_everything.log
 fi
 exit 0
